e6f7927e5a3d405be69f19f1a29730d0e1278122
Peter G <kpg141260@live.de>
2020-04-02 14:22:49 +0800

    Updates after cleanup

--- scr/TimerMillis.cpp
+++ scr/TimerMillis.cpp
@@ -1,41 +1,20 @@
-#include <timer.h>
-#include "Arduino.h"
-/*
-#include <stdio.h>
-#include <string.h>
-#include <inttypes.h>
-*/
-/** Function TimerMillis *****************************************************
-**
-**  Constructor for the TimerMillis class. Sets the timeout and starts timer.
-**
-**  Parameters: unsigned long timeout
-**              The timeout value for this instance. The value should be 
-**              greater than 0, if value is <= 0 the constructor will use the 
-**              default value TMR_DEFAULT_TO, which is 10 ms.
-**  Returns:    void - nothing
-*******************************************************************************/
+#include <Arduino.h>
+#include "timermillis.h"
+
 TimerMillis::TimerMillis (unsigned long to) {
     if (to > 1) {
         _timeout = to;
     }
     else {
-        _timeout = TMR_DEFAULT_TO;
+        _timeout = DEFAULT_TO;
     }
-    _timer_active = true;
+    _timer_active = ACTIVE;
     #ifdef DEBUG
         _debug_counter = 0;
     #endif
     _last_millis = millis();
 }
-/** Function IsElapsed ********************************************************
-**
-**  Function checks whether the given Timer is done or not.
-**
-**  Parameters: void - none
-**  Returns:    DONE        - if timeout reached (true),
-**              NOT_DONE    - if timeout not yet reached (false).
-*******************************************************************************/
+
 bool TimerMillis::IsElapsed (void) {
     if (_timer_active) {
         if (millis() < _last_millis) {
@@ -47,66 +26,31 @@ bool TimerMillis::IsElapsed (void) {
             #ifdef DEBUG
                 _debug_counter++;
             #endif
-            return true;
+            return ELAPSED;
         }
         else
-            return false;
+            return NOT_ELAPSED;
     }
-    return false;
+    return NOT_ACTIVE;
 }
-/** Function Reset ************************************************************
-**
-**  Function 'resets' and activates timer.
-**
-**  Parameters: void - none
-**  Returns:    void - nothing
-*******************************************************************************/
+
+bool TimerMillis::IsActive (void) {
+    return _timer_active;
+}
+
 void TimerMillis::Reset (void) {
-    _timer_active = true;
+    _timer_active = ACTIVE;
     _last_millis = millis();
 }
-/** Function Start ************************************************************
-**
-**  Function activates timer.
-**
-**  Parameters: void - none
-**  Returns:    DONE        - if timeout reached (true),
-**              NOT_DONE    - if timeout not yet reached (false).
-*******************************************************************************/
+
 void TimerMillis::Start() {
-    _timer_active = true;
+    _timer_active = ACTIVE;
 }
-/** Function Stop *************************************************************
-**
-**  Function dectivates Timer.
-**
-**  Parameters: void - none
-**  Returns:    DONE        - if timeout reached (true),
-**              NOT_DONE    - if timeout not yet reached (false).
-*******************************************************************************/
+
 void TimerMillis::Stop (){
-    _timer_active = false;
+    _timer_active = NOT_ACTIVE;
 }
-/** Function GetTimeout ***********************************************************
-**
-**  Function returns this Timers timeout value - Getbimeoutck.
-**
-**  Parameters: void - none
-**  Returns:    unsigned long - this timers time out value.
-*******************************************************************************/
-unsigned long TimerMillis::GetTimeout (void) {
-    return _timeout;
-}
-/** Function Set **************************************************************
-**
-**  Set a new timeout value at runtime, the value could be retrieved through
-**  a menu or serial input.
-**
-**  Parameters: unsigned long
-**              the new timeout value should, be greater than 0, if value
-**              is < 0 the function will continue using the old value.
-**  Returns:    void - nothing
-*******************************************************************************/
+
 void TimerMillis::Set (unsigned long to) {
     if (to > 1) {
         _timeout = to;
@@ -117,18 +61,23 @@ void TimerMillis::Set (unsigned long to) {
     _last_millis = millis();
 }
 
-/******************************************************************************
-**  The follwing functions where intruduced for troubleshooting of an initial 
-**  issue I had with the class, I left them in, but you may just delete them or 
-**  comment them out.
-*******************************************************************************/
 unsigned long TimerMillis::Get (void) {
     return millis ();
 }
+
+unsigned long TimerMillis::GetTimeout (void) {
+    return _timeout;
+}
+
 /* Functions returns value (difference) of millis - _last_millis */
 unsigned long TimerMillis::GetDelta (void) {
     return _dif;
 }
+
+unsigned long TimerMillis::GetDeltaToTarget (void) {
+    return _timeout - _dif;
+}
+
 /* Function returns value of _last_millis */
 unsigned long TimerMillis::GetLastMillis (void) {
     return _last_millis;
--- scr/TimerMillis.h
+++ scr/TimerMillis.h
@@ -1,39 +1,167 @@
-/*  TIMER_MILLIS.H ***************************************************
+/*  
+**  TIMER_MILLIS.H **************************************************
 **  Library for Ardunio timer functions based on the Millis counter
 **
 **  Library created by Peter Gossler
 **  Latest released version 0.1
 **  Release date: 21/04/2017
-**********************************************************************/
+*********************************************************************
+*/
 
-#ifndef TIMER_MILLIS
-    #define TIMER_MILLIS
-    #define TMR_DEFAULT_TO 10
+#ifndef TIMER_MILLIS_H_
+    #define TIMER_MILLIS_H_
+
+    #define DEFAULT_TO  100
+    #define ELAPSED     TRUE
+    #define NOT_ELAPSED FALSE
+    #define NOT_ACTIVE  FALSE
+    #define ACTIVE      TRUE
 
     class TimerMillis {
         public:
+            /** Function TimerMillis *****************************************************
+            **
+            **  Constructor for the TimerMillis class. Sets the timeout and starts timer.
+            **
+            **  Parameters: unsigned long timeout
+            **              The timeout value for this instance. The value should be 
+            **              greater than 0, if value is <= 0 the constructor will use the 
+            **              default value TMR_DEFAULT_TO, which is 10 ms.
+            **  Returns:    void - nothing
+            *******************************************************************************/
             TimerMillis (unsigned long to);
+
+            /** Function IsElapsed ********************************************************
+            **
+            **  Function checks whether the given Timer is done or not.
+            **
+            **  Parameters: void - none
+            **  Returns:    DONE        - if timeout reached (true),
+            **              NOT_DONE    - if timeout not yet reached (false).
+            *******************************************************************************/
             bool IsElapsed ();
+
+            /** Function IsActive *********************************************************
+            **
+            **  Function checks whether the given Timer is active or not.
+            **
+            **  Parameters: void - none
+            **  Returns:    ACTIVE      - the timer is active (TRUE),
+            **              NOT_ACTIVE  - the timer is not active (FALSE).
+            *******************************************************************************/
+            bool IsActive ();
+
+            /** Function Reset ************************************************************
+            **
+            **  Function 'resets' and activates timer.
+            **
+            **  Parameters: void - none
+            **  Returns:    void - nothing
+            *******************************************************************************/
             void Reset ();
+
+            /** Function Start ************************************************************
+            **
+            **  Function activates timer.
+            **
+            **  Parameters: void - none
+            **  Returns:    DONE        - if timeout reached (true),
+            **              NOT_DONE    - if timeout not yet reached (false).
+            *******************************************************************************/
             void Start ();
+            
+            /** Function Stop *************************************************************
+            **
+            **  Function dectivates Timer.
+            **
+            **  Parameters: void - none
+            **  Returns:    DONE        - if timeout reached (true),
+            **              NOT_DONE    - if timeout not yet reached (false).
+            *******************************************************************************/
             void Stop ();
+            
+
+            /** Function Set **************************************************************
+            **
+            **  Set a new timeout value at runtime.
+            **
+            **  Parameters: unsigned long
+            **              the new timeout value should, be greater than 0, if value
+            **              is < 0 the function will continue using the old value.
+            **  Returns:    void - nothing
+            *******************************************************************************/
             void Set (unsigned long to);
+
+           
+            /** Function Get **************************************************************
+            **
+            **  Retrieves the current millis value.
+            **
+            **  Parameters: void - none
+            **  Returns:    unsigned long - the current value of the millis counter.
+            *******************************************************************************/
             unsigned long Get ();
+
+
+            /** Function GetTimeout ********************************************************
+            **
+            **  Function returns this instances target timeout value.
+            **
+            **  Parameters: void - none
+            **  Returns:    unsigned long - this instances timeout value.
+            *******************************************************************************/ 
             unsigned long GetTimeout();
+
+
+            /** Function GetDelta *********************************************************
+            **
+            **  Function returns elapsed millis since last checking.
+            **
+            **  Parameters: void - none
+            **  Returns:    unsigned long - the elapsed millis since last checking.
+            *******************************************************************************/ 
             unsigned long GetDelta();
+
+
+            /** Function GetDeltaToTarget *************************************************
+            **
+            **  Function returns delta of current millis to target.
+            **
+            **  Parameters: void - none
+            **  Returns:    unsigned long - delta millis to target.
+            *******************************************************************************/ 
+            unsigned long GetDeltaToTarget();
+
+
+            /** Function GetLastMillis ****************************************************
+            **
+            **  Function returns millis since last checking.
+            **
+            **  Parameters: void - none
+            **  Returns:    unsigned long - millis since last checking.
+            *******************************************************************************/ 
             unsigned long GetLastMillis ();
+
             #ifdef DEBUG
+            /** Function GetDebugCounter **************************************************
+            **
+            **  Function returns a counter that increments everytime the method IsElapsed()
+            **  is called.
+            **
+            **  Parameters: void - none
+            **  Returns:    unsigned long - millis since last checking.
+            *******************************************************************************/ 
                 unsigned long GetDebugCounter ();
             #endif
             //void TimerSetCallBack ()
         private:
+            bool            _timer_active;
             unsigned long   _timeout;
             unsigned long   _last_millis;
             unsigned long   _dif;
             #ifdef DEBUG
                 unsigned long   _debug_counter;
             #endif
-            bool            _timer_active;
     };
-#endif // #ifdef TIMER_MILLIS
+#endif // #ifdef TIMER_MILLIS_H_
 
